e6ebfa3c998f9e8f4a23456f9a1a13edf903bac6
Dorottya <kamaras.dorottya@gmail.com>
2021-02-15 20:59:13 +0100

    routing is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { from } from 'rxjs';
+
+const appRoutes: Routes = [
+ { 
+   path: "",
+   component: HomeComponent
+ },
+ {
+   path: "heroes", 
+   component: HeroesComponent
+ }
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +32,9 @@ import { HeroesComponent } from './heroes/heroes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule, 
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
